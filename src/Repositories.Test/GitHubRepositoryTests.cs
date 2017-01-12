16f3eb57a5927d2129a4200242cb307781b83982
Palvinder Bidwall <pal@bidwall.co.uk>
2017-01-12 10:47:43 +0000

    Added tests to GitHubRepositoryTests and some refactoring.

--- src/Repositories.Test/GitHubRepositoryTests.cs
+++ src/Repositories.Test/GitHubRepositoryTests.cs
@@ -12,8 +12,10 @@ namespace Repositories.Test
     {
         private readonly IHttpClientHelper _mockHttpClientHelper;
         private readonly GitHubRepository _gitHubRepository;
+
         private const string UserName = "username";
-        private const string Url = "url";
+        private const string RepoUrl = "url";
+        private readonly string _userUrl = $"{GitHubRepository.GitHubUri}/users/{UserName}";
 
 
         public GitHubRepositoryTests()
@@ -26,22 +28,49 @@ namespace Repositories.Test
         public void GetDetailsForUser_ConstructsGitHubUrl()
         {
             //Arrange
-            var url = $"{GitHubRepository.GitHubUri}/users/{UserName}";
-
-            _mockHttpClientHelper.Arrange(x => x.GetDataFromUrl<User>(url)).Returns(new User());
+            _mockHttpClientHelper.Arrange(x => x.GetDataFromUrl<User>(_userUrl)).Returns(new User());
 
             //Act
             _gitHubRepository.GetDetailsForUser(UserName);
         }
 
         [Test]
+        public void GetDetailsForUser_ReturnsUser()
+        {
+            //Arrange
+            var testUser = new User{Name = "Test"};
+
+            _mockHttpClientHelper.Arrange(x => x.GetDataFromUrl<User>(Arg.AnyString)).Returns(testUser);
+
+            //Act
+            var user = _gitHubRepository.GetDetailsForUser(UserName);
+
+            //Assert
+            Assert.That(user.Name, Is.EqualTo(testUser.Name));
+        }
+
+        [Test]
         public void GetReposForUserFromUrl_GetReposForUrl()
         {
             //Arrange
-            _mockHttpClientHelper.Arrange(x => x.GetDataFromUrl<IEnumerable<Repo>>(Url)).Returns(new Repo[] {});
+            _mockHttpClientHelper.Arrange(x => x.GetDataFromUrl<IEnumerable<Repo>>(RepoUrl)).Returns(new Repo[] {});
 
             //Act
-            _gitHubRepository.GetReposForUserFromUrl(Url);
+            _gitHubRepository.GetReposForUserFromUrl(RepoUrl);
+        }
+
+        [Test]
+        public void GetReposForUserFromUrl_ReturnsRepos()
+        {
+            //Arrange
+            IEnumerable<Repo> testRepos = new[] { new Repo{ Name = "Test" } };
+            _mockHttpClientHelper.Arrange(x => x.GetDataFromUrl<IEnumerable<Repo>>(Arg.AnyString)).Returns(testRepos);
+
+            //Act
+            var repos = _gitHubRepository.GetReposForUserFromUrl(RepoUrl);
+
+            //Assert
+            Assert.That(repos, Is.EquivalentTo(testRepos));
         }
     }
 }
