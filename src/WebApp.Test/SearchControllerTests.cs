48f576e8ffe9d007af3f525e5d34366428bcdd54
Palvinder Bidwall <pal@bidwall.co.uk>
2017-02-03 10:39:40 +0000

    Added tests for SearchController

--- src/WebApp.Test/SearchControllerTests.cs
+++ src/WebApp.Test/SearchControllerTests.cs
@@ -1,10 +1,48 @@
-﻿using NUnit.Framework;
+﻿using System.Web.Mvc;
+using NUnit.Framework;
+using WebApp.Controllers;
+using WebApp.Models;
 
 namespace WebApp.Test
 {
     [TestFixture]
     public class SearchControllerTests
     {
+        private SearchController _searchController;
 
+        [OneTimeSetUp]
+        public void OneTimeSetup()
+        {
+            _searchController = new SearchController();
+        }
+                
+        [Test]
+        public void IndexPost_Invalid_ReturnsViewWithModel()
+        {
+            //Arrange
+            _searchController.ModelState.AddModelError("", "");
+
+            //Act
+            var result = _searchController.Index(new SearchViewModel()) as ViewResult;
+
+            //Assert
+            Assert.That(result, Is.Not.Null);
+            Assert.That(string.IsNullOrEmpty(result.ViewName), Is.True);
+        }
+
+        [Test]
+        public void IndexPost_ValidModel_RedirectsToUsersIndex()
+        {
+            //Arrange
+            _searchController.ModelState.Clear();
+
+            //Act
+            var result = _searchController.Index(new SearchViewModel()) as RedirectToRouteResult;
+
+            //Assert
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result.RouteValues["action"], Is.EqualTo("Index"));
+            Assert.That(result.RouteValues["controller"], Is.EqualTo("Users"));
+        }
     }
 }
