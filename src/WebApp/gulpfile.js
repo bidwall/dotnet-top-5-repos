75fd3fd392e5b3bd01bb1998ecba4bb89d23682a
Palvinder Bidwall <pal@bidwall.co.uk>
2017-02-10 14:34:24 +0000

    Use gulp-load-plugins to lazy load gulp plugins

--- src/WebApp/gulpfile.js
+++ src/WebApp/gulpfile.js
@@ -1,33 +1,32 @@
 ﻿/// <binding AfterBuild='default' Clean='clean' />
 var gulp = require('gulp');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var del = require('del');
-var mainBowerFiles = require('main-bower-files');
-var cleanCss = require('gulp-clean-css');
-var flatten = require('gulp-flatten');
+
+var plugins = require("gulp-load-plugins")({
+  pattern: ['gulp-*', 'gulp.*', 'main-bower-files', 'del'],
+  replaceString: /\bgulp[\-.]/
+});
 
 gulp.task('clean', function () {
-    return del(['Scripts/**/*', 'Content/site.min.css', 'fonts/**/*']);
+    return plugins.del(['Scripts/**/*', 'Content/site.min.css', 'fonts/**/*']);
 });
 
 gulp.task('scripts', function () {    
-    return gulp.src(mainBowerFiles('**/*.js'))
-        .pipe(concat('site.min.js'))
-        .pipe(uglify())
+    return gulp.src(plugins.mainBowerFiles('**/*.js'))
+        .pipe(plugins.concat('site.min.js'))
+        .pipe(plugins.uglify())
         .pipe(gulp.dest('Scripts/'));
 });
 
 gulp.task('styles', function () {
     return gulp.src(['bower_components/bootstrap/dist/css/bootstrap.css', 'Content/*'])
-            .pipe(concat('site.min.css'))
-            .pipe(cleanCss())
+            .pipe(plugins.concat('site.min.css'))
+            .pipe(plugins.cleanCss())
             .pipe(gulp.dest('Content/'));
 });
 
 gulp.task('fonts', function() {
    return gulp.src(['bower_components/**/*.{eot,svg,ttf,woff,woff2}'])
-            .pipe(flatten())
+            .pipe(plugins.flatten())
             .pipe(gulp.dest('fonts/'));
 });
 
@@ -40,4 +39,4 @@ gulp.task('default', ['clean'], function() {
 
 
 // Watch
-gulp.watch('Content/site.css', ['styles']);
+// gulp.watch('Content/site.css', ['styles']);
