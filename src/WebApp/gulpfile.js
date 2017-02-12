7ff77e83c30db887594aeda6534bb412c65ff268
Palvinder <pal@bidwall.co.uk>
2017-02-12 20:47:45 +0000

    Use configuration object to specify values
    Move src, dest and bundle values into single object so modifications can be made at one place

--- src/WebApp/gulpfile.js
+++ src/WebApp/gulpfile.js
@@ -1,42 +1,61 @@
 ﻿/// <binding AfterBuild='default' Clean='clean' />
 var gulp = require('gulp');
-
 var plugins = require("gulp-load-plugins")({
   pattern: ['gulp-*', 'gulp.*', 'main-bower-files', 'del'],
   replaceString: /\bgulp[\-.]/
 });
 
+
+// configuration
+var config = {
+    scripts: {
+        bundle: 'site.min.js',
+        dest: 'Scripts/',
+        glob: '**/*.js'
+    },
+    styles: {
+        bundle: 'site.min.css',
+        dest: 'Content/',
+        src: ['bower_components/bootstrap/dist/css/bootstrap.css', 'Content/*']
+    },    
+    fonts: {
+        dest: 'fonts/',
+        src: ['bower_components/**/*.{eot,svg,ttf,woff,woff2}']
+    }
+}
+
+
+// tasks
 gulp.task('clean', function () {
     return plugins.del(['Scripts/**/*', 'Content/site.min.css', 'fonts/**/*']);
 });
 
 gulp.task('scripts', function () {    
-    return gulp.src(plugins.mainBowerFiles('**/*.js'))
-        .pipe(plugins.concat('site.min.js'))
+    return gulp.src(plugins.mainBowerFiles(config.scripts.glob))
+        .pipe(plugins.concat(config.scripts.bundle))
         .pipe(plugins.uglify())
-        .pipe(gulp.dest('Scripts/'));
+        .pipe(gulp.dest(config.scripts.dest));
 });
 
 gulp.task('styles', function () {
-    return gulp.src(['bower_components/bootstrap/dist/css/bootstrap.css', 'Content/*'])
-            .pipe(plugins.concat('site.min.css'))
+    return gulp.src(config.styles.src)
+            .pipe(plugins.concat(config.styles.bundle))
             .pipe(plugins.cleanCss())
-            .pipe(gulp.dest('Content/'));
+            .pipe(gulp.dest(config.styles.dest));
 });
 
 gulp.task('fonts', function() {
-   return gulp.src(['bower_components/**/*.{eot,svg,ttf,woff,woff2}'])
+   return gulp.src(config.fonts.src)
             .pipe(plugins.flatten())
-            .pipe(gulp.dest('fonts/'));
+            .pipe(gulp.dest(config.fonts.dest));
 });
 
 gulp.task('build', ['scripts', 'styles', 'fonts']);
 
-// Default task
 gulp.task('default', ['clean'], function() {
     gulp.start('build');
 });
 
 
-// Watch
-// gulp.watch('Content/site.css', ['styles']);
+// Watches
+gulp.watch('Content/site.css', ['styles']);
